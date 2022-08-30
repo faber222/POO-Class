a3168668345159aa612942b988e7ff1b26ec8123
faber222 <faberbernardo78@gmail.com>
2022-08-30 09:16:45 -0300

    first java code

---
+++ OlaMundo.java
@@ -0,0 +1,9 @@
+// para compilar javac {ArquivoJava.java}
+// para executar java {ArquivoJava.java}
+
+public class OlaMundo {
+    public static void main(String[] args) {
+        // imprimindo a mensagem na tela
+        System.out.println("Ola mundo!");
+    }
+}
