063be038c6a1008b72b71eca43c2415c7d8cf9e7
faber222 <faberbernardo78@gmail.com>
2022-09-20 18:16:11 -0300

    Rebase the entire folder

--- Finished/CodigosWebTeste/campoMinado/campoMinado.java
+++ Finished/CodigosWebTeste/campoMinado/campoMinado.java
@@ -1,7 +1,8 @@
+package CodigosWebTeste.campoMinado;
 public class campoMinado {
     public static void main(String[] args) {
         Jogo jogo = new Jogo();
-
+        System.out.println(jogo);
     }
 
 }
