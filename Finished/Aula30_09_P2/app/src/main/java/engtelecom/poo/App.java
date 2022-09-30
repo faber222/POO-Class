8ba1d22450acd0550029e4c48316c833e8a923ca
faber222 <faberbernardo78@gmail.com>
2022-09-30 11:29:23 -0300

    Learning how to use Class in Class

--- Finished/Aula30_09_P2/app/src/main/java/engtelecom/poo/App.java
+++ Finished/Aula30_09_P2/app/src/main/java/engtelecom/poo/App.java
@@ -1,6 +1,23 @@
 package engtelecom.poo;
 
 public class App {
+    // atributo
+    private String nome = "POO";
+
+    // ou
+    public App(String n) {
+        this.nome = n;
+    }
+
+    // método
+    public void falar() {
+        System.out.println("oi: " + this.nome);
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
     public static void main(String[] args) {
         Tempo relogio = new Tempo(12, 20, 30);
         Tempo relogio2 = new Tempo(10, 20, 30);
@@ -11,5 +28,11 @@ public class App {
         relogio.setMinuto(50);
         relogio.setSegundo(23);
         System.out.println(relogio);
+
+        // como App é uma classe, podemos criar objetos, e atribuir métodos e atributos
+        App a = new App("STD");
+        App b = new App(a.getNome());
+        a.falar();
+        b.falar();
     }
 }
