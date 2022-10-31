ff1968bf1c054ae55f3e9fd599a4fbcc548010ec
faber222 <faberbernardo78@gmail.com>
2022-10-31 00:17:00 -0300

    Finalizado - addPessoa, removePessoa e addEmail

--- Finished/Aula25_10/app/src/main/java/engtelecom/poo/Agenda.java
+++ Finished/Aula25_10/app/src/main/java/engtelecom/poo/Agenda.java
@@ -6,17 +6,42 @@ public class Agenda {
     private ArrayList<Pessoa> contatos;
 
     public Agenda() {
+        this.contatos = new ArrayList<>();
     }
 
     public boolean addPessoa(Pessoa p) {
+        if (!verificaExistencia(this.contatos, p)) {
+            this.contatos.add(p);
+            return true;
+        }
         return false;
     }
-    
+
     public boolean removePessoa(String n, String s) {
+        int i = 0;
+        for (Pessoa x : this.contatos) {
+            if (x.getNome() == n && x.getSobrenome() == s) {
+                this.contatos.remove(i);
+                return true;
+            }
+            i++;
+        }
         return false;
     }
 
     public boolean addEmail(String r, String e, int pIndex) {
+        int x = 0;
+        if (!this.contatos.isEmpty()) {
+            for (Pessoa list : this.contatos) {
+                if (x == pIndex) {
+                    if (list.addEmail(r, e)) {
+                        this.contatos.set(pIndex, list);
+                        return true;
+                    }
+                }
+                x++;
+            }
+        }
         return false;
     }
 
@@ -39,11 +64,19 @@ public class Agenda {
     public boolean updateTelefone(String r, String n, int pIndex) {
         return false;
     }
-    
-    @Override
-    public String toString() {
-        return "Agenda [contatos=" + contatos + "]";
+
+    private boolean verificaExistencia(ArrayList<Pessoa> lista, Pessoa p) {
+        for (Pessoa x : this.contatos) {
+            if (x.getNome() == p.getNome() && x.getSobrenome() == p.getSobrenome()) {
+                return true;
+            }
+        }
+        return false;
     }
 
-    
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
 }
